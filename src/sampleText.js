9603815bb73d58297a44becb31eca48dd1366946
Alex Simisi <smsxander@gmail.com>
2019-09-14 14:17:08 +0200

    création d'un éditeur Markdown

---
+++ src/sampleText.js
@@ -0,0 +1,2 @@
+export const sampleText =
+  '# Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar une ligne vide.\n\nLaissé deux espaces à la fin d\'une ligne pour  \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`, ~~rayé~~.\n\nListe:\n\n  * pommes\n  * oranges\n  * poires\n\nListe numérotée:\n\n  1. jambon\n  2. cornichon\n  3. pain\n\n *[Alex Simisi Nta](https://www.linkedin.com/in/alex-simisi-nta-98b67a168/)* \n\n Lien automatique : https://www.linkedin.com/in/alex-simisi-nta-98b67a168/ \n\n```\n console.log("hello"); \n``` ';
